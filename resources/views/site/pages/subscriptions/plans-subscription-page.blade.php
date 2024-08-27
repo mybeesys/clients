97f53641932d54bfb47609d0f17f2bf02217d0c8
leengithub <145831839+leengithub@users.noreply.github.com>
2024-08-27 09:34:12 +0300

    styleing in the plan subscription page.

--- resources/views/site/pages/subscriptions/plans-subscription-page.blade.php
+++ resources/views/site/pages/subscriptions/plans-subscription-page.blade.php
@@ -6,7 +6,7 @@
                 <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
                         <div class="card-header">
-                            <h4 class="my-0 font-weight-normal">{{ $plan->name }}</h4>
+                            <h4 class="my-0 font-weight-normal">{{ ucwords($plan->name) }}</h4>
                         </div>
                         <div class="card-body">
                             <h1 class="card-title pricing-card-title">${{ $plan->price }}
