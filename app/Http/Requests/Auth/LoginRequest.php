8b9578da77ca3d51a81450fe2cd4caaa536d47de
mhd_amen042 <miladalmani03@gmail.com>
2025-07-18 11:44:49 +0300

    Ensure the login request is not rate limited

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -28,8 +28,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email', 'max:255', 'exists:users,email'],
-            'password' => ['required', Password::defaults()]
+            'email' => [ 'max:255', 'exists:users,email'],
         ];
     }
 
